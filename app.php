d233ab2859abf5fdaa9ca562bc8678682ba9aeca
Arunabh Das <arunabhdas@gmail.com>
2015-12-22 03:20:16 -0500

    Added includes for header and footer

--- app.php
+++ app.php
@@ -1,29 +1,9 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Froyo</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
-  <script src="app.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.7/semantic.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.7/semantic.min.css" />
-  <link rel="stylesheet" href="app.css" />
-
-</head>
-<body>
-  <p> Welcome to Froyo</p>
-  <div class="ui red menu">
-    <h3 class="header item">Home</h3>
-    <a class="active item">Profile</a>
-    <a class="item">About</a>
-    <a class="item">Contact</a>
-    <a class="item">Products</a>
-  </div>
-
+<?php include_once('header.php'); ?>
   <div class="ui four column grid">
-    <div class="column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    <div class="column">
+    Login <a href="login.html">here</a>
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -48,5 +28,4 @@
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
   </div>
-</body>
-</html>
+<?php include_once('footer.php'); ?>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Froyo</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+  <script src="app.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.7/semantic.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.7/semantic.min.css" />
+  <link rel="stylesheet" href="app.css" />
+
+</head>
+<body>
+  <h1> Welcome to Froyo</h1>
+  <div class="ui red menu">
+    <h3 class="header item">Home</h3>
+    <a class="active item">Profile</a>
+    <a class="item">About</a>
+    <a class="item">Contact</a>
+    <a class="item">Products</a>
+  </div>
